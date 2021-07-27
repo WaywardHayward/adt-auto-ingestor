1c46b6e73ea304632be5b957bdb214c812bdaf58
Alex Hayward <haywardalex@microsoft.com>
2021-07-27 15:49:21 +0100

    Added Configuration for TwinId Path from the Twin Identifiers.

--- Models/Constants.cs
+++ Models/Constants.cs
@@ -6,5 +6,7 @@ namespace adt_auto_ingester.Models
     public class Constants
     {
         public const string ADT_URL_SETTING = "INGESTION_TWIN_URL";
+        public const string ADT_TWIN_IDENTIFIERS = "INGESTION_TWIN_IDENTIFIERS";
+        public const string INGESTION_EVENT_HUB_NAME = "INGESTION_EVENTHUB_NAME";
     }
 }
